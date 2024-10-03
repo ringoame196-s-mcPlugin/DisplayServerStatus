2e78371546753ecb570b64fa101657ff5c5f4c08
りんご飴196 <kazumac41@gmail.com>
2024-10-03 23:13:49 +0900

    [追加] BOT起動 定期アップデート開始

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -2,15 +2,37 @@ package com.github.ringoame196_s_mcPlugin
 
 import com.github.ringoame196_s_mcPlugin.commands.Command
 import com.github.ringoame196_s_mcPlugin.commands.TabCompleter
+import com.github.ringoame196_s_mcPlugin.managers.DiscordManager
+import com.github.ringoame196_s_mcPlugin.managers.StatusManager
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
     override fun onEnable() {
         super.onEnable()
         val plugin = this
+        val discordManager = DiscordManager()
+        val statusManager = StatusManager(plugin)
         saveDefaultConfig() // configファイル生成
+        val updateFrequency = config.getInt("updateFrequency") // 更新頻度
+
+        discordManager.bootBOT(plugin) {
+            // DiscordBOTが起動した後に実行される
+            statusManager.startFixedTermUpdate(updateFrequency) // 定期アップデート開始
+        }
+
+        // コマンド登録
         val command = getCommand("displayserverstatus")
-        command!!.setExecutor(Command())
+        command!!.setExecutor(Command(plugin))
         command.tabCompleter = TabCompleter()
+    }
+
+    override fun onDisable() {
+        val plugin = this
+        if (Data.JDA != null) {
+            val statusManager = StatusManager(plugin)
+            statusManager.update(false) // ステータスをオフラインにする
+            Data.JDA?.shutdown() // botをシャットダウンする
+        }
+        super.onDisable()
     }
 }
