7e816d219e757314123e579af3edd9494c5fab20
りんご飴196 <kazumac41@gmail.com>
2024-10-03 23:12:49 +0900

    [追加] data

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Data.kt
@@ -0,0 +1,11 @@
+package com.github.ringoame196_s_mcPlugin
+
+import net.dv8tion.jda.api.JDA
+
+object Data {
+    var JDA: JDA? = null
+
+    const val STATUS_TITLE = ":desktop: サーバーステータス "
+    const val STATUS_ONLINE = "[オンライン]"
+    const val STATUS_OFFLINE = "[オフライン]"
+}
