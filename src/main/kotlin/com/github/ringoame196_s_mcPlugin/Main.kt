ae6e9ebaecb58fd81e5c5712dea8891ef70faefd
りんご飴196 <kazumac41@gmail.com>
2024-10-03 17:17:33 +0900

    [追加] configファイル

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -1,13 +1,14 @@
 package com.github.ringoame196_s_mcPlugin
 
-import org.bukkit.plugin.java.JavaPlugin
 import com.github.ringoame196_s_mcPlugin.commands.Command
 import com.github.ringoame196_s_mcPlugin.commands.TabCompleter
+import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
     override fun onEnable() {
         super.onEnable()
         val plugin = this
+        saveDefaultConfig() // configファイル生成
         val command = getCommand("displayserverstatus")
         command!!.setExecutor(Command())
         command.tabCompleter = TabCompleter()
